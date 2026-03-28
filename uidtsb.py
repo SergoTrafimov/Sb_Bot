34f5d70508bac6cbd0ebde26c1a52efeec6f54eb
SergoTrafimov <salfisher2222@gmail.com>
2026-03-28 12:45:52 +0300

    uu

--- uidtsb.py
+++ uidtsb.py
@@ -5,9 +5,9 @@ API_HASH = 'ef9c85d5d2c2c9dc4d79ae281ec83a8d'
 
 async def get_user_simple(username):
     # Данные вашего MTProto прокси
-    proxy_host = "rimuruproject.ru"
-    proxy_port = 8443
-    secret = "dd248cdb278a96b01d27f67787135791da"  # например, "ee1234567890abcdef..."
+    proxy_host = "217.16.30.89"
+    proxy_port = 443
+    secret = "dd6fab08b49908b2b3c055eb6e7055ff07"  # например, "ee1234567890abcdef..."
 
     # Кортеж из трёх элементов (без указания типа)
     proxy = (proxy_host, proxy_port, secret)
