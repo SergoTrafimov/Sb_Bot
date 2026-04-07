b6b23a2c44326506e09e1727cb046052abd0431d
SergoTrafimov <salfisher2222@gmail.com>
2026-04-07 23:01:40 +0300

    proxy

--- uidtsb.py
+++ uidtsb.py
@@ -5,9 +5,9 @@ API_HASH = 'ef9c85d5d2c2c9dc4d79ae281ec83a8d'
 
 async def get_user_simple(username):
     # Данные вашего MTProto прокси
-    proxy_host = "217.16.30.89"
+    proxy_host = "mt.geodema.network"
     proxy_port = 443
-    secret = "dd6fab08b49908b2b3c055eb6e7055ff07"  # например, "ee1234567890abcdef..."
+    secret = "6fab08b49908b2b3c055eb6e7055ff07"  # например, "ee1234567890abcdef..."
 
     # Кортеж из трёх элементов (без указания типа)
     proxy = (proxy_host, proxy_port, secret)
